a9230106b406e6e2d8347822fe9cb0ab6f20dd9a
Nischay Pro <NischayPro@yahoo.co.in>
2016-09-12 17:54:09 +0530

    Test

--- Strange/Form1.vb
+++ Strange/Form1.vb
@@ -1,3 +1,20 @@
-﻿Public Class Form1
+﻿Imports System.Runtime.InteropServices
 
+Public Class Form1
+    Dim hgloballist As New List(Of IntPtr)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim hglobal As IntPtr
+        hglobal = Marshal.AllocHGlobal(10485760)
+        hgloballist.Add(hglobal)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        For Each Item As IntPtr In hgloballist
+            Marshal.FreeHGlobal(Item)
+        Next
+    End Sub
 End Class
